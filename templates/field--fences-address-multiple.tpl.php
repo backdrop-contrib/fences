b3df4608383ebe8900293937c80d12ed1c375f1a
Greg Leroux <gleroux02@282323.no-reply.drupal.org>
2011-09-23 16:49:13 -0500

    Adds the address template files

---
+++ templates/field--fences-address-multiple.tpl.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Support for the <pre> and <code> elements
+ * for Drupal multivalue fields
+ * http://developers.whatwg.org/grouping-content.html#the-pre-element
+ * http://developers.whatwg.org/text-level-semantics.html#the-code-element
+ *
+ * These semantics may not be right for your content. We suggest you read the spec and customize this as needed. You can make a copy of this file, put it in your theme, and edit it (and clear the cache) --Êand Drupal will you your copy instead of ours. (Don't alter this copy, however!)
+ * 
+ */
+?>
+<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php if ($element['#label_display'] == 'inline'): ?>
+    <span class="field-label <?php print $title_attributes; ?>">
+      <?php print $label; ?>:
+    </span>
+  <?php elseif ($element['#label_display'] == 'above'): ?>
+    <h3 class="field-label <?php print $title_attributes; ?>">
+      <?php print $label; ?>
+    </h3>
+  <?php endif; ?>
+  
+
+  <?php foreach ($items as $delta => $item) : ?>
+    <address class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
+      <?php print render($item); ?>
+    </address>
+  <?php endforeach; ?>
+
+</div>
---
+++ templates/field--fences-address.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Support for the <address> element
+ * for a single Drupal field
+ * http://developers.whatwg.org/grouping-content.html#the-figure-element
+ *
+ * These semantics may not be right for your content. We suggest you read the spec and customize this as needed. You can make a copy of this file, put it in your theme, and edit it (and clear the cache) --Êand Drupal will you your copy instead of ours. (Don't alter this copy, however!)
+ *
+ */
+?>
+<address class="<?php print $classes; ?> clearfix"<?php print $attributes . $item_attributes[0]; ?>>
+
+  <?php if (!$label_hidden) : ?>
+    <span class="field-label <?php print $title_attributes; ?>">
+      <?php print $label; ?>:
+    </span>
+  <?php endif; ?>
+  
+  <?php print render($items[0]); ?>
+
+</address>
