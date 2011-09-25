edbed56b5e1ec0bef921dac46560743a2d295c2b
Patrick Teglia <pteglia+drupal@gmail.com>
2011-09-25 11:02:08 -0700

    Working on theme functions

--- templates/field--fences-small.tpl.php
+++ templates/field--fences-small.tpl.php
@@ -8,8 +8,8 @@
  * These semantics may not be right for your content. We suggest you read the spec and customize this as needed. You can make a copy of this file, put it in your theme, and edit it (and clear the cache) --Êand Drupal will you your copy instead of ours. (Don't alter this copy, however!)
  *
  */
-?>
-<small class="<?php print $classes; ?> clearfix"<?php print $attributes . $item_attributes[0]; ?>>
+?><h1>This is the small tpl</h1>
+<small class="<?php print $classes; ?> clearfix"<?php // print $attributes . $item_attributes[0]; ?>>
 
   <?php if ($element['#label_display'] == 'inline'): ?>
     <span class="field-label <?php print $title_attributes; ?>">
