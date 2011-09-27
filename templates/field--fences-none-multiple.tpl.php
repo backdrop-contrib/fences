0059b73b61833bd23afb138b2f192d12671d4bfc
Jen Simmons <jen@jensimmons.com>
2011-09-27 11:33:25 -0400

    making the none wrapper really none for multiple fields
    Signed-off-by: Jen Simmons <jen@jensimmons.com>

--- templates/field--fences-none-multiple.tpl.php
+++ templates/field--fences-none-multiple.tpl.php
@@ -20,7 +20,5 @@
   <?php endif; ?>
   
   <?php foreach ($items as $delta => $item) : ?>
-    <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
       <?php print render($item); ?>
-    </div>
   <?php endforeach; ?>
