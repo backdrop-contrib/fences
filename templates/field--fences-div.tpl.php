a40cdbc13ed28016db222c7b723f70e6d36d0ef5
Jen Simmons <jen@jensimmons.com>
2011-09-27 11:22:00 -0400

    fixing typo in the div tpl
    Signed-off-by: Jen Simmons <jen@jensimmons.com>

--- templates/field--fences-div.tpl.php
+++ templates/field--fences-div.tpl.php
@@ -5,7 +5,7 @@
  * for a single Drupal field
  * http://developers.whatwg.org/grouping-content.html#the-div-element
  *
- * These semantics may not be right for your content. We suggest you read the spec and customize this as needed. You can make a copy of this file, put it in your theme, and edit it (and clear the cache) --Êand Drupal will you your copy instead of ours. (Don't alter this copy, however!)
+ * These semantics may not be right for your content. We suggest you read the spec and customize this as needed. You can make a copy of this file, put it in your theme, and edit it (and clear the cache) -- and Drupal will you your copy instead of ours. (Don't alter this copy, however!)
  *
  */
 ?>
