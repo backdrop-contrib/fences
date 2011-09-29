e3de45a884ef687209996e0f090f5053b02bbd6f
JohnAlbin <virtually.johnalbin@gmail.com>
2011-09-29 18:17:47 +0800

    by JohnAlbin: Remove debug line from small template.

--- templates/field--fences-small.tpl.php
+++ templates/field--fences-small.tpl.php
@@ -8,7 +8,7 @@
  * These semantics may not be right for your content. We suggest you read the spec and customize this as needed. You can make a copy of this file, put it in your theme, and edit it (and clear the cache) -- and Drupal will you your copy instead of ours. (Don't alter this copy, however!)
  *
  */
-?><h1>This is the small tpl</h1>
+?>
 <small class="<?php print $classes; ?> clearfix"<?php // print $attributes . $item_attributes[0]; ?>>
 
   <?php if ($element['#label_display'] == 'inline'): ?>
