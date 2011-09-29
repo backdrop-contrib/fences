1db2dce959b6919720c0d31930f9b0471d5aa9e5
JohnAlbin <virtually.johnalbin@gmail.com>
2011-09-29 18:19:05 +0800

    by JohnAlbin: Fix indentation in "none" templates.

--- templates/field--fences-none-multiple.tpl.php
+++ templates/field--fences-none-multiple.tpl.php
@@ -8,16 +8,16 @@
  *
  */
 ?>
-  <?php if ($element['#label_display'] == 'inline'): ?>
-    <span class="field-label <?php print $title_attributes; ?>">
-      <?php print $label; ?>:
-    </span>
-  <?php elseif ($element['#label_display'] == 'above'): ?>
-    <h3 class="field-label <?php print $title_attributes; ?>">
-      <?php print $label; ?>
-    </h3>
-  <?php endif; ?>
+<?php if ($element['#label_display'] == 'inline'): ?>
+  <span class="field-label <?php print $title_attributes; ?>">
+    <?php print $label; ?>:
+  </span>
+<?php elseif ($element['#label_display'] == 'above'): ?>
+  <h3 class="field-label <?php print $title_attributes; ?>">
+    <?php print $label; ?>
+  </h3>
+<?php endif; ?>
 
-  <?php foreach ($items as $delta => $item) : ?>
-      <?php print render($item); ?>
-  <?php endforeach; ?>
+<?php foreach ($items as $delta => $item) : ?>
+  <?php print render($item); ?>
+<?php endforeach; ?>
--- templates/field--fences-none.tpl.php
+++ templates/field--fences-none.tpl.php
@@ -8,15 +8,14 @@
  *
  */
 ?>
-  <?php if ($element['#label_display'] == 'inline'): ?>
-    <span class="field-label <?php print $title_attributes; ?>">
-      <?php print $label; ?>:
-    </span>
-  <?php elseif ($element['#label_display'] == 'above'): ?>
-    <h3 class="field-label <?php print $title_attributes; ?>">
-      <?php print $label; ?>
-    </h3>
-  <?php endif; ?>
+<?php if ($element['#label_display'] == 'inline'): ?>
+  <span class="field-label <?php print $title_attributes; ?>">
+    <?php print $label; ?>:
+  </span>
+<?php elseif ($element['#label_display'] == 'above'): ?>
+  <h3 class="field-label <?php print $title_attributes; ?>">
+    <?php print $label; ?>
+  </h3>
+<?php endif; ?>
 
-  <?php print render($items[0]); ?>
-
+<?php print render($items[0]); ?>
