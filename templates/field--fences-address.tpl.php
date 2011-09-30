e4072d21b0a372d623f95c2f9231a53d7e4bd293
JohnAlbin <virtually.johnalbin@gmail.com>
2011-09-30 12:38:33 +0800

    by JohnAlbin: Fix label display in address template.

--- templates/field--fences-address.tpl.php
+++ templates/field--fences-address.tpl.php
@@ -13,10 +13,14 @@
 ?>
 <address class="<?php print $classes; ?> clearfix"<?php print $attributes . $item_attributes[0]; ?>>
 
-  <?php if (!$label_hidden) : ?>
+  <?php if ($element['#label_display'] == 'inline'): ?>
     <span class="field-label <?php print $title_attributes; ?>">
       <?php print $label; ?>:
     </span>
+  <?php elseif ($element['#label_display'] == 'above'): ?>
+    <h3 class="field-label <?php print $title_attributes; ?>">
+      <?php print $label; ?>
+    </h3>
   <?php endif; ?>
 
   <?php print render($items[0]); ?>
