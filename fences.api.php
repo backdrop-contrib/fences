070e38ee4e28f897a88f3f663e8320f27268ceb7
JohnAlbin <virtually.johnalbin@gmail.com>
2011-11-06 01:57:59 +0800

    by JohnAlbin: Add element and groups keys to hook_fences_suggestion_info().

--- fences.api.php
+++ fences.api.php
@@ -20,14 +20,21 @@ function hook_fences_suggestion_info() {
   // The suggestions for the "field" theme hook.
   $fences['field'] = array(
     // This key will be appended to THEMEHOOK__fences_ to make the theme hook
-    // suggestion. field__fences_p in this case. The corresponding template
-    // files should be named field--fences-p.tpl.php and
-    // field--fences-p-multiple.tpl.php.
+    // suggestion, field__fences_p in this case. The corresponding template
+    // files should be have all underscores changed to dashes and be named
+    // field--fences-p.tpl.php and field--fences-p-multiple.tpl.php.
     'p' => array(
-      // The label used in the UI when selecting a suggestion.
-      'label' => t('p'),
-      // The description used in the UI when selecting a suggestion.
-      'description' => t('A paragraph tag. Multiple values are each wrapped in a <p>.'),
+      // The label used in the UI when selecting a suggestion. The label must
+      // only contain English words to enable multilingual translations.
+      'label' => t('paragraph'),
+      // The HTML element(s) used by the suggestion. This will be added to the
+      // label in the UI to provide additional context. If multiple elements are
+      // used they should be seperated by spaces, e.g. 'pre code'.
+      'element' => 'p',
+      // A short description used in the UI when selecting a suggestion.
+      'description' => t('A paragraph; multiple values are each wrapped in a <p>'),
+      // An array of groups to place the element into.
+      'groups' => array(t('Block-level')),
     ),
   );
   return $fences;
